216a1f7e558cf67621485935a289ff226f6a3b0f
aseelabusaifan <adeelmohammsd@gmail.com>
2021-07-14 18:14:02 +0300

    add form

--- js/app.js
+++ js/app.js
@@ -18,9 +18,9 @@
 
 //     calccustomerseachhour: function(){
 //         for (let index = 0; i < hour.length; i++) {
-        
+
 //             this.calccustomerseachhour.push(random(this.mincustomers,this.mincustomers))
-            
+
 //         }
 //     },
 //     calccookieseachhour:function (){
@@ -64,53 +64,88 @@
 // seattle.calccookieseachhour();
 // // console.log(seattle.cookieseachhour);
 // seattle.render();
-  
 
 
 
- let hours=['6am',7am','8am','9am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7am' ];
+
+let hours = ['6am', '7am', '8am', '9am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7am'];
 
 // from w3schools
- function random (min, max) {
-return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
-  let shops[];
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+let shops = [];
 
-  function Shop (location,mincustomers,maxcustomers,avgcookies){
-  this.location=location;
-  this.mincustomers=mincustomers;
-  this.maxcustomers=maxcustomers;
-  this.avgcookies=avgcookies;
-  this.totalcookiedperday=0;
-this.customerseachhour=[];
-this.cookiesechhour=[];
-  }
-  console.log (this);
+function Shop(location, mincustomers, maxcustomers, avgcookies) {
+  this.location = location;
+  this.mincustomers = mincustomers;
+  this.maxcustomers = maxcustomers;
+  this.avgcookies = avgcookies;
+  this.totalcookiedperday = 0;
+  this.customerseachhour = [];
+  this.cookiesechhour = [];
   shops.push(this);
-  shop.prototype.calacustomerseachhour=function
-  (){
-      for (let i = 0; i < hours.length; i++) {this.calacustomerseachhour.push(random(this,mincustomers))
-        
-          
-      }
-      this.cookieseachhour.push(math.floor (this.customerseachhour[i]*this.avgcookies));
-      this.total+=this.cookieseachhour[i];
-          }
-          let seattle=new Shop('seattle',23,65,6.3);
+}
 
-          let tokyo=new Shop('tokyo',3,24,1.2);
+console.log(this);
 
-          let dubai=new Shop('dubai',11,38,3.7);
-          let paris=new Shop('paris',20,38,2.3);
-          let lima=new Shop('lima',2,16,4.6);
+Shop.prototype.calacustomerseachhour = function
+  () {
+  for (let i = 0; i < hours.length; i++) {
+    this.customerseachhour.push(random(this.mincustomers, this.maxcustomers))
 
-        console.log(shops);
-        for (let i = 0; i < shops.length; i++) {
-            shops[i] customerseachhour
-            shops[i]cookieseachhour
-            
-            
-        }
+    this.cookiesechhour.push(Math.floor(this.customerseachhour[i] * this.avgcookies));
+    // this.total += this.  this.cookiesechhour = [];
+    // [i]
+
+  }
+
+
+}
+let seattle = new Shop('seattle', 23, 65, 6.3);
+
+let tokyo = new Shop('tokyo', 3, 24, 1.2);
+
+let dubai = new Shop('dubai', 11, 38, 3.7);
+let paris = new Shop('paris', 20, 38, 2.3);
+let lima = new Shop('lima', 2, 16, 4.6);
+
+
+for (let i = 0; i < shops.length; i++) {
+  shops[i].calacustomerseachhour();
+
+
+}
+console.log(shops);
+
+
+let table = document.getElementById('table')
+
+function header() {
+  let tr = document.createElement("tr")
+  table.appendChild(tr)
+  let td = document.createElement("td")
+  tr.appendChild(td)
+  td.textContent = ""
+  
+  for (let i = 0; i < hours.length; i++) {
+    let td2=document.createElement("td")
+    tr.appendChild(td2)
+    td2.textContent = hours[i]
+    
+  }
+
+
+}
+header();
+
+let form=document.getElementById('form');
+form.addEventListener('submit',submitter);
+function submitter(event){
+  event.prevented fault();
+  console.log(event);
+}
+
 
 
 
